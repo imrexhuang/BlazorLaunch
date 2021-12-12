e8cc1c5bd8ee0621f06ba76f4c4eaef723a96694
Rex Huang <im@rex.tw>
2021-12-12 22:49:52 +0800

    Razor 元件 - 繼承 CodeBehind
    練習
    https://gist.github.com/vulcanlee/ccfce68ff963380bdb0387ea61eb0666

---
+++ BlazorLaunch/Pages/CounterInheritBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorLaunch.Pages
+{
+    // https://gist.github.com/vulcanlee/ccfce68ff963380bdb0387ea61eb0666
+    // 練習:Razor 元件 - 繼承 CodeBehind
+
+    // 這個類別需要繼承 [ComponentBase] 類別
+    public class CounterInheritBase : ComponentBase
+    {
+        public int currentCount = 0;
+
+        public void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
