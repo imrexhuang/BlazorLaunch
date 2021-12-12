5f8e763c55779f11225943537e69de0520644037
Rex Huang <im@rex.tw>
2021-12-13 07:50:41 +0800

    C# 程式碼設計方法 (BL02) : Razor 元件 - Partial 類別
    練習
    https://gist.github.com/vulcanlee/ccfce68ff963380bdb0387ea61eb0666

---
+++ BlazorLaunch/Pages/CounterPartialClass.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorLaunch.Pages
+{
+    // https://gist.github.com/vulcanlee/ccfce68ff963380bdb0387ea61eb0666
+    public partial class CounterPartialClass
+    {
+        private int currentCount = 0;
+
+        void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+
+}
